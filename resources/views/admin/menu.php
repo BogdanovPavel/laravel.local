2351ae1e76fc8fcb45b45bd021c1752866131269
BogdanovPavel <tor-3000@yandex.ru>
2020-08-13 23:13:34 +0500

    Добавил Admin панель
    Добавил въюхи и контролер

---
+++ resources/views/admin/menu.php
@@ -0,0 +1,4 @@
+<a href="<?=route('Home')?>">Главная сайт</a>
+<a href="<?=route('admin.index')?>">Главная Админка</a>
+<a href="<?=route('admin.test1')?>">Test 1</a>
+<a href="<?=route('admin.test2')?>">Test 2</a>
---
+++ resources/views/admin/test1.php
@@ -0,0 +1,5 @@
+<?php
+include_once "menu.php";
+?>
+
+<h2>Тэст 1</h2>
